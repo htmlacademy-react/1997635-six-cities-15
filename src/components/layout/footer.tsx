28cd9f90f5b07722040616feffc69d57ea33df25
lilyanovi <zauzolkova.lilia@ya.ru>
2024-02-24 22:58:06 +0300

    fix! props in footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,20 +1,14 @@
 import Logo from '../ui/logo';
 
-type FooterProps = {
-  shouldRenderFooter: boolean;
-}
-
-function Footer ({shouldRenderFooter} : FooterProps) {
-  if (shouldRenderFooter){
-    return (
-      <footer className="footer container">
-        <Logo
-          isHeader={false}
-          linkClassName=''
-        />
-      </footer>
-    );
-  }
+function Footer () {
+  return (
+    <footer className="footer container">
+      <Logo
+        isHeader={false}
+        linkClassName=''
+      />
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -33,7 +33,7 @@ function Layout () {
         shouldRenderUser={shouldRenderUser}
       />
       <Outlet />
-      <Footer shouldRenderFooter={shouldRenderFooter}/>
+      {shouldRenderFooter && <Footer/>}
     </div>
   );
 }
