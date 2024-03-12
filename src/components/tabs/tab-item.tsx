cade6ad800b6a9ed13251f77fb8fad2cdc549e15
lilyanovi <zauzolkova.lilia@ya.ru>
2024-03-12 19:52:38 +0300

    add active tabs

--- src/components/tabs/tab-item.tsx
+++ src/components/tabs/tab-item.tsx
@@ -1,11 +1,19 @@
+import { Locations } from '../../const';
+
 type TabItemProps = {
-  place: string;
+  activeTab: keyof typeof Locations;
+  place: Locations;
+  handleTabActive: (place: keyof typeof Locations) => void;
 }
 
-function TabItem ({place} : TabItemProps) : JSX.Element {
+function TabItem ({activeTab, place, handleTabActive} : TabItemProps) : JSX.Element {
   return (
     <li className="locations__item">
-      <a className="locations__item-link tabs__item" href="#">
+      <a
+        className={`locations__item-link tabs__item${activeTab === place ? ' tabs__item--active' : ''}`}
+        href="#"
+        onClick={() =>handleTabActive(place)}
+      >
         <span>{place}</span>
       </a>
     </li>
